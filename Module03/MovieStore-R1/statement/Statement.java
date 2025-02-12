cac827477013979a1d5a0b9548d5956d9b5dd2fa
joseph.isaacs <joseph.isaacs@mymail.champlain.edu>
2025-02-12 11:31:53 -0500

    Pull up the methods from children

--- Module03/MovieStore-R1/statement/Statement.java
+++ Module03/MovieStore-R1/statement/Statement.java
@@ -1,7 +1,22 @@
 package statement;
 
 import main.Customer;
+import main.Rental;
 
 public abstract class Statement {
-    public abstract String makeStatement(Customer customer);
+    public String makeStatement(Customer customer) {
+        String result = getHeaderString(customer);
+
+        for (Rental rental : customer.getRentals()) {
+            result += getEachRentalString(rental);
+        }
+
+        result += getFooterString(customer);
+
+        return result;
+    }
+
+    public abstract String getFooterString(Customer customer);
+    public abstract String getEachRentalString(Rental rental);
+    public abstract String getHeaderString(Customer customer);
 }
